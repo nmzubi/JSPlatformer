770f6eaf6dccc39a9b38274474f2f5671d214122
nmzubi <nmzubi@knights.ucf.edu>
2016-10-24 00:05:13 -0400

    changed colors

--- code/canvasdisplay.js
+++ code/canvasdisplay.js
@@ -60,9 +60,9 @@ CanvasDisplay.prototype.clearDisplay = function() {
   if (this.level.status == "won")
     this.cx.fillStyle = "rgb(68, 191, 255)";
   else if (this.level.status == "lost")
-    this.cx.fillStyle = "rgb(44, 136, 214)";
+    this.cx.fillStyle = "rgb(102, 51, 102)";
   else
-    this.cx.fillStyle = "rgb(52, 166, 251)";
+    this.cx.fillStyle = "rgb(188, 156, 299)";
   this.cx.fillRect(0, 0,
                    this.canvas.width, this.canvas.height);
 };
